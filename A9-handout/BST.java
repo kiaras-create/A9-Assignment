a5abd2222d4cbae4b215acb9b30beb2e3b270e79
cyang-05 <chiashiiyang@gmail.com>
2025-11-19 22:56:48 -0500

    BST stubs added

--- A9-handout/BST.java
+++ A9-handout/BST.java
@@ -5,8 +5,62 @@
  * @version Fall 2025
  */
 public class BST<E extends Comparable<E>> extends BinaryTree<E> implements BST_Ops<E> {
+    
     public BST(E data) {
         super(data);
+    }
+
+    /**
+     *  Returns the node of the given element, or null if not found
+     *
+     *  @param query The element to search
+     *  @return the node of the given element, or null if not found
+     */
+    public BST<E> lookup(E data) {
+
+    }
+
+    /**
+     *  Inserts a new node into the tree
+     *
+     *  @param data The element to insert
+     */
+    public void insert(E data) {
+
+    }
+
+    /**
+     *  Deletes the specified element from the tree
+     *  Returns the modified tree because the root node 
+     *  may have changed
+     *  
+     *  @param evictee The element to delete
+     *  @return tree as modified
+     */
+    public BST<E> deleteWithCopyLeft(E evictee) {
+
+    }
+
+    /**
+     *  Apply left rotation
+     *  Returns the modified tree because the root node 
+     *  may have changed
+     *
+     *  @return tree as modified
+     */
+    public BST<E> rotateLeft() {
+
+    }
+
+    /**
+     *  Apply right rotation
+     *  Returns the modified tree because the root node 
+     *  may have changed
+     *
+     *  @return tree as modified
+     */
+    public BST<E> rotateRight() {
+
     }
     
 }
