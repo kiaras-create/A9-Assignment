c04fa1bba73043ef2676930c76f52524067ae7ee
kiaras-create <ksunilsingh@smith.edu>
2025-12-18 23:19:20 -0500

    BST Tests

--- A9-handout/BSTTests.java
+++ A9-handout/BSTTests.java
@@ -55,4 +55,91 @@ public class BSTTests {
         tree.insert(7);
         verifyBT(tree, gt2);
     }
+   // Delete a leaf using copy left
+    @Test
+    public void TestBSTDeleteCopyLeft(){
+        BST<Integer> tree = new BST<>(5);
+        tree.insert(6);
+        tree.insert(10);
+        tree.insert(4);
+        tree = tree.deleteWithCopyLeft(4);
+        Integer[][] expected = {
+            {4},
+            {null, 6},
+            {null, null, null,10}
+        };
+        verifyBT(tree, expected);
+    }
+
+    // Delete node with two children
+    @Test
+        public void TestBSTDeleteCopyLeftTwo(){
+        BST<Integer> tree = new BST<>(10);
+        tree.insert(5);
+        tree.insert(15);
+        tree.insert(4);
+        tree.insert(1);
+        tree.insert(17);
+        tree.insert(14);
+        tree.insert(7);
+
+        tree = tree.deleteWithCopyLeft(5);
+        Integer[][] expected = {
+            {10},
+            {4,15},
+            {1,7,14,17},
+        };
+        verifyBT(tree, expected);
+    }
+    // Delete node with one child
+    @Test
+    public void TestBSTDeleteCopyLeftOne(){
+        BST<Integer> tree = new BST<>(10);
+        tree.insert(5);
+        tree.insert(15);
+        tree.insert(4);
+        tree.insert(1);
+
+        tree = tree.deleteWithCopyLeft(4);
+        Integer[][] expected = {
+            {10},
+            {5,15},
+            {1, null, null,null}
+            
+        };
+        verifyBT(tree,expected);
+
+    }
+
+    @Test 
+    public void TestBSTRotateLeft(){
+        BST<Integer> tree = new BST<>(10);
+        //tree.insert(5);
+        tree.insert(15);
+
+        tree = tree.rotateLeft();
+        Integer[][] expected = {
+            {15},
+            {10, null}
+        };
+           verifyBT(tree, expected);
+        
+    }
+
+    public void TestBSTRotateRight(){
+        BST<Integer> tree = new BST<Integer>(10);
+        tree.insert(5);
+
+        tree = tree.rotateRight();
+        Integer[][] expected = {
+            {5},
+            {null, 10}
+        };
+        verifyBT(tree, expected);
+    }
+
+
+
+
+
 }
